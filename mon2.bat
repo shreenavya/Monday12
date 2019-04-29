6ae2d36f2cd9b4a938c1bd189938c9f38aed9863
shreenavya <managerm911@gmail.com>
2019-04-29 05:32:25 -0700

    test

---
+++ mon2.bat
@@ -0,0 +1,3 @@
+set classpath=D:\Monday\Monday1\lib\*;D:\Monday\Monday1\bin
+java org.testng.TestNG testng.xml
+pause;
---
+++ src/Mon1/MO.java
@@ -0,0 +1,14 @@
+package Mon1;
+
+import org.testng.annotations.Test;
+
+public class MO {
+	@Test
+	public void v2()
+	
+	{
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
